27085006e636981d0f2cd94e85767c8b7327f82f
Your Name <you@example.com>
2016-08-30 09:56:09 +0000

    datalog query running in the same container as message passing

--- client/src/js/modules/chat/containers/timecontainer.js
+++ client/src/js/modules/chat/containers/timecontainer.js
@@ -4,6 +4,22 @@ import Chat from '../Chat'
 import d from 'datascript'
 import {createStore} from 'redux'
 
+
+  // fires when we receive a message
+  const receiveChatMessage = (message) => {
+    const { user } = message
+console.log(message)
+    if (isMe(user)) return // prevent echoing yourself (TODO: server could handle this i guess?)
+console.log({
+  text: message.body,
+  name: message.user,
+  image: avatar,
+  position: 'left',
+  date: moment()
+})
+  }
+
+
 console.log("root2")
 
 const defaultState = {time: new Date().toString()};
@@ -34,6 +50,21 @@ setInterval(() => {
 console.log("test3")
 
 const onPropsChange = (props, onData) => {
+
+
+// yes, i'm 41 years old.
+const NAMES = ['Girl', 'Boy', 'Horse', 'Poo', 'Face', 'Giant', 'Super', 'Butt', 'Captain', 'Lazer']
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
+const getRandomName = () => NAMES[getRandomInt(0, NAMES.length)]
+const getRandomUser = () => `${ getRandomName() }${ getRandomName() }${ getRandomName() }`
+const user = getRandomUser()
+const isMe = (someUser) => user === someUser
+const avatar = { uri: 'https://facebook.github.io/react/img/logo_og.png' }
+
+
+    const chat = Chat(user, receiveChatMessage)
+    chat.send("yow")
+
 
 var db = d.db_with(d.empty_db(), [[":db/add", 1, "name", "Ivan"],
                          [":db/add", 1, "age", 17]])
