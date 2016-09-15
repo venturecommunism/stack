b46a40b0f9f988a0b8ac66b306cacd3274a75f73
Your Name <you@example.com>
2016-09-15 17:05:11 +0000

    preparing for react datascript queries

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DBConnProvider } from './dbConnProvider'
+import createDBConn from './createDBConn';
+import { AllUserEdges, AllUsers, AllUsersFromIndex, FollowerTree } from './components';
+
 import App from './App';
 import './index.css';
 
