832a8a38cd1573c282c7c1ebda22d7afaaacfbef
Your Name <you@example.com>
2016-09-16 08:46:41 +0000

    getting closer to transacting state everywhere

--- client/src/modules/datascript/commands/followertree.js
+++ client/src/modules/datascript/commands/followertree.js
@@ -1,6 +1,6 @@
 export default {
-  addfollowerofjane({transact}) {
-    transact([{
+  addfollowerofjane({transactstate}) {
+    transactstate([{
       ':db/id': -1,
       name: `Follower of Jane ${new Date().getTime()}`,
       follows: ['name', 'Jane']
