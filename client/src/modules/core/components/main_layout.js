95520a6c252e1a458008be20c96878df6cc28dca
Your Name <you@example.com>
2016-09-16 07:16:59 +0000

    navigation

--- client/src/modules/core/components/main_layout.js
+++ client/src/modules/core/components/main_layout.js
@@ -3,16 +3,17 @@ import logo from './images/fivepetal.svg'
 import './styles/app.css'
 import './styles/index.css'
 
-const Layout = ({content = () => null }) => (
+const Layout = ({links = () => null, content = () => null }) => (
   <div className='App'>
     <div className='App-header'>
       <img src={logo} className='App-logo' alt='logo' />
       <h2>Under Construction</h2>
+      { links() }
     </div>
     <p className='App-intro'>
       To get started, edit <code>src/modules/datascript/components/index.js</code> and save to reload.
     </p>
-    {content()}
+    { content() }
   </div>
 )
 
