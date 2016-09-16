4421f0c433176097ca813a393d2fb61c449be35d
Your Name <you@example.com>
2016-09-16 03:06:10 +0000

    misc

--- client/src/configs/context.js
+++ client/src/configs/context.js
@@ -1,11 +1,9 @@
 import url from './url'
 
-import {mount} from 'react-mounter'
-import {Socket} from 'phoenix'
+import { Socket } from 'phoenix'
 
 export const initContext = () => {
   return {
-    mount,
     socket: new Socket(url)
   }
 }
