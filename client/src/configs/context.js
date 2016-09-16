0863faf6977311638f63248afbf6c8442f9b838b
Your Name <you@example.com>
2016-09-16 11:47:10 +0000

    appears to work but not actually changing values

--- client/src/configs/context.js
+++ client/src/configs/context.js
@@ -1,18 +1,10 @@
 import url from './url'
 import createDBConn from '../lib/createDBConn'
-import datascript from 'datascript'
 import { Socket } from 'phoenix'
-
-const conn = createDBConn()
-
-const transactstate = (data, txMsg) => {
-  datascript.transact(conn, data, txMsg);
-}
 
 export const initContext = () => {
   return {
     socket: new Socket(url),
-    transactstate,
-    conn,
+    conn: createDBConn(),
   }
 }
--- client/src/modules/datascript/commands/followertree.js
+++ client/src/modules/datascript/commands/followertree.js
@@ -1,5 +1,16 @@
+import datascript from 'datascript'
+
 export default {
-  addfollowerofjane({transactstate}) {
+  addfollowerofjane(context) {
+
+function transactstate ({conn}, data, txMsg) {
+//const {conn} = context
+console.log("context", context, "context")
+console.log("conn", context.conn, "conn")
+  datascript.transact(context.conn, data, txMsg);
+console.log("newp")
+}
+console.log("ping")
     transactstate([{
       ':db/id': -1,
       name: `Follower of Jane ${new Date().getTime()}`,
