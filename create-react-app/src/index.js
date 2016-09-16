a78dad0ad7319919ccc97e1f40059597f2ae0818
Your Name <you@example.com>
2016-09-16 00:19:43 +0000

    cleaning up entrypoint

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,24 +1,19 @@
-/*
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import {createApp} from 'mantra-core'
 import {initContext} from './configs/context'
 
 const context = initContext()
 const app = createApp(context)
 
+import coreModule from './modules/datascript'
+
 app.loadModule(coreModule)
 app.init()
-*/
 
-//import Time from './modules/chat/components/time.jsx'
 import Root from './modules/chat/containers/timecontainer'
-//import Root from './modules/chat/components/Redux.jsx'
-//import Root from './modules/chat/components/Root.jsx'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import DataScript from './modules/datascript/index'
-
-//const RootContainer = Root(Time)
-const DataScriptContainer = Root(DataScript)
+const DataScriptContainer = Root(coreModule)
 
 ReactDOM.render(<DataScriptContainer />, document.getElementById('root'))
+
