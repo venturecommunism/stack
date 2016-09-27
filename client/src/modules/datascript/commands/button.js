37ff060f4a7c1bde7a9f015c6bbb33a5593cf866
Your Name <you@example.com>
2016-09-27 03:55:45 +0000

    general idea of transacting jsonld

--- client/src/modules/datascript/commands/button.js
+++ client/src/modules/datascript/commands/button.js
@@ -1,9 +1,8 @@
-import {promises} from 'jsonld'
+import { promises } from 'jsonld'
 import parser from 'rdf-nx-parser'
 
 export default {
   import({conn, transact}) {
-    console.log(conn)
 
     var doc = {
       "@context": "http://json-ld.org/contexts/person.jsonld",
@@ -17,27 +16,47 @@ export default {
 
     return promise
     .then((ntriples) => {
-
-      console.log(ntriples)
+      var name = ''
+      var born = ''
+      var spouse = ''
 
       ntriples.split(" .").map( triple => transacttriple(triple+" .") )
 
       function transacttriple(triple) {
-        console.log("triple", triple)
+        if (!parser.parseTriple(triple)) {
+          return
+        }
         var parsedtriple = parser.parseTriple(triple)
 
-        console.log(parsedtriple)
+        const jsonld = {
+          name: "http://xmlns.com/foaf/0.1/name",
+          born: "http://schema.org/birthDate",
+          spouse: "http://schema.org/spouse"
+        }
 
-        transact(conn, [
-
-          {
-            ':db/id': -1,
-            name: `${parsedtriple.object.value}`,
-            follows: ['name', 'Jane']
-          }
-
-        ])
+        if (parsedtriple.predicate.value === jsonld.name) {
+          name = parsedtriple.object.value
+        }
+        if (parsedtriple.predicate.value === jsonld.born) {
+          born = parsedtriple.object.value
+        }
+        if (parsedtriple.predicate.value === jsonld.spouse) {
+          spouse = parsedtriple.object.value
+        }
       }
+
+      transact(conn, [
+
+        {
+          ':db/id': -1,
+          "@id": "http://dbpedia.org/resource/John_Lennon",
+          "name": `${name}`,
+          "born": `${born}`,
+          "spouse": `${spouse}`,
+//          "follows": ['name', 'Jane']
+        }
+
+      ])
 
     })
   },
