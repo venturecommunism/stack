73c6ead6022f7632cee436ad4ca445167d16977a
Your Name <you@example.com>
2016-09-27 18:36:23 +0000

    transacting multiple expanded and compacted records

--- client/src/modules/datascript/commands/button.js
+++ client/src/modules/datascript/commands/button.js
@@ -3,31 +3,31 @@ import { promises } from 'jsonld'
 export default {
   import({conn, transact}) {
 
-    var doc = {
+    const context = "http://json-ld.org/contexts/person.jsonld"
+
+    var data = [{
       "@context": "http://json-ld.org/contexts/person.jsonld",
       "@id": "http://dbpedia.org/resource/John_Lennon",
       "name": "John Lennon",
       "born": "1940-10-09",
       "spouse": "http://dbpedia.org/resource/Cynthia_Lennon"
-    }
+    },
+    {
+      "@context": "http://json-ld.org/contexts/person.jsonld",
+      "@id": "http://dbpedia.org/resource/Will_Rogers",
+      "name": "Will Rogers",
+      "born": "1879-11-04",
+      "spouse": "Betty Blake"
+    }]
 
-    const promiseexpand = promises.expand(doc)
-
+    const promiseexpand = promises.expand(data)
     return promiseexpand
     .then((expanded) => {
-      const promisecompact = promises.compact(expanded, "http://json-ld.org/contexts/person.jsonld")
-
+      const promisecompact = promises.compact(expanded, context)
       return promisecompact
       .then((compacted) => {
-        transact(conn, [
-
-          {
-            ':db/id': -1,
-            ...compacted
-          }
-
-        ])
-
+        compacted["@graph"].forEach( c => c["@context"] = context )
+        transact(conn, compacted["@graph"])
       })
     })
   },
