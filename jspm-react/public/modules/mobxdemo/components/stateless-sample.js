e36f8a3ede7ac46173d75c2ba0736fa00cd2e287
anonymous <anonymous>
2016-09-30 22:50:42 -0400

    retry

---
+++ jspm-react/public/modules/mobxdemo/components/stateless-sample.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react'
+import counter from '../stores/example-store'
+import {observer} from 'mobx-react'
+
+const Test = (props) => {
+  return <div onClick={() => counter.value++}>click to increase counter value: {counter.value}</div>
+}
+
+Test.propTypes = {
+  name: PropTypes.string
+}
+
+export default observer(Test)
---
+++ jspm-react/run-tests.js
@@ -0,0 +1,27 @@
+require('systemjs')
+System.config({
+  baseURL: 'public/'
+})
+require('./public/jspm.browser')
+require('./public/jspm.config')
+
+require('babel-register')
+
+const Mocha = require('mocha')
+const glob = require('glob')
+const mocha = new Mocha()
+
+glob('test/**/*.js', function (err, files) {
+  var file, i, len
+  if (err) {
+    throw err
+  }
+  for (i = 0, len = files.length; i < len; i++) {
+    file = files[i]
+    mocha.addFile(file)
+  }
+  mocha.ui('bdd')
+  return mocha.run(function (failures) {
+    return process.exit(failures)
+  })
+})
