cc364561a4f89111154b3eeade79cdf2fd0ed909
anonymous <anonymous>
2016-10-14 09:52:09 -0400

    transaction logging

--- client/src/modules/datascript/commands/checkout.js
+++ client/src/modules/datascript/commands/checkout.js
@@ -1,3 +1,5 @@
+import datascript from 'datascript'
+
 export default {
   buy({conn, transact}, e) {
     // console.log('conn', conn)
@@ -5,10 +7,21 @@ export default {
     // console.log('event', e)
 
     // the following creates and deletes some nodes if and only if the first entity id it created is 4. subsequent clicks create entities 5, 6, 7, etc.
-    transact(conn, [[':db/add', -1, 'shoppingcart/thing', 'xxyyy'],
-                    [':db/add', -1, 'state/otherthing', 'stuffinit']])
-    transact(conn, [[':db/retract', 4, 'shoppingcart/thing', 'xxyyy'],
-                    [':db/retract', 4, 'state/otherthing', 'stuffinit']])
+
+    var log = []
+    var meta = []
+    datascript.listen(conn, function(report) {
+                              log.push(report.tx_data)
+                              meta.push(report.tx_meta)
+    })
+
+    console.log(transact(conn, [[':db/add', -1, 'shoppingcart/thing', 'xxyyy'],
+                    [':db/add', -1, 'state/otherthing', 'stuffinit']]))
+    console.log(transact(conn, [[':db/retract', 4, 'shoppingcart/thing', 'xxyyy'],
+                    [':db/retract', 4, 'state/otherthing', 'stuffinit']]))
+
+    console.log(log)
+    console.log(meta)
     return {
       ':db/id': -1,
       name: `Follower of Jane ${new Date().getTime()}`,
