40c9e8fdf26b5d13a2e8612971b7addb51e73d52
anonymous <anonymous>
2016-10-17 06:41:21 -0400

    misc cleanup

--- client/src/modules/ec_checkout/components/index.jsx
+++ client/src/modules/ec_checkout/components/index.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 import ActionsMapper from '../../core/containers/actionsmapper'
 
-import shoppingCartQuery from '../../ec_checkout/queries/shoppingcart'
-import CheckoutComponent from '../../ec_checkout/components/checkout.jsx'
+import shoppingCartQuery from '../queries/shoppingcart'
+import CheckoutComponent from './checkout.jsx'
 const Checkout = shoppingCartQuery(ActionsMapper('checkout', CheckoutComponent))
 
 export default () => (
-  <div>
-    <Checkout />
-  </div>
+  <Checkout />
 )
 
