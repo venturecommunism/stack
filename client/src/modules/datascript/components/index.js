f3cc75d4520852d93d2d4944dd6a95c6c6420fd6
anonymous <anonymous>
2016-10-17 02:34:04 -0400

    separating main component into multiple components

--- client/src/modules/datascript/components/index.js
+++ client/src/modules/datascript/components/index.js
@@ -8,37 +8,13 @@ import shoppingCartQuery from '../../ec_checkout/queries/shoppingcart'
 import CheckoutComponent from '../../ec_checkout/components/checkout.jsx'
 const Checkout = shoppingCartQuery(ActionsMapper('checkout', CheckoutComponent))
 
-import JWT from '../../clientjwt/components/jwt.jsx'
-
-import AllUsersComponent from './allusers'
-import AllUserEdgesComponent from './alluseredges'
-import FollowerTreeComponent from './followertree'
-
-import ImportUI from '../../jsonld/components/importui.jsx'
-const ActionsImportUI = ActionsMapper('importui', ImportUI)
-
-import allUserQuery from '../queries/alluser'
-const AllUsers = allUserQuery(AllUsersComponent)
-
-import allUsersFromIndex from '../queries/allusersfromindex'
-const AllUsersFromIndex = allUsersFromIndex(AllUsersComponent)
-
-import allUserEdgesQuery from '../queries/alluseredges'
-const AllUserEdges = allUserEdgesQuery(AllUserEdgesComponent)
-
-import followerTreePullQuery from '../queries/followertreepull'
-const FollowerTree = followerTreePullQuery(ActionsMapper('followertree', FollowerTreeComponent))
+//import Subindex from './subindex'
 
 const Root = () => (
   <div>
     <Dashboard />
     <Checkout />
-    <JWT />
-    <ActionsImportUI />
-    <AllUsers />
-    <AllUsersFromIndex />
-    <AllUserEdges />
-    <FollowerTree entityIds={[['name', 'Jane']]} />
+    {/* <Subindex /> */}
   </div>
 )
 
