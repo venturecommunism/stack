f5a950fd3e1f254f522414ee991f6adee0f0f0f5
anonymous <anonymous>
2016-10-18 06:42:31 -0400

    misc cleanup

--- client/src/modules/datascript/routes.js
+++ client/src/modules/datascript/routes.js
@@ -4,9 +4,7 @@ import { DBConnProvider } from '../../lib/react-datascript'
 
 import Navigation from '../layout/components/navigation'
 import MainLayout from '../layout/components/main_layout'
-//import Container from '../../modules/sockets/containers/timecontainer'
 import Feed from './components/index'
-//const FeedPage = Container(Feed)
 
 export default function (injectDeps, context, actions) {
   const conn = context.conn
--- client/src/modules/sockets/components/time.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default ({msg}) => (
-  <div><b>Time is</b>: {msg}</div>
-)
