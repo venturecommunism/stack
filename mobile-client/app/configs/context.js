c3eaae80afe95b514520fc433c997b9563546b5e
anonymous <anonymous>
2016-10-25 16:39:21 -0400

    react-native android client still working

--- mobile-client/app/configs/context.js
+++ mobile-client/app/configs/context.js
@@ -1,3 +1,9 @@
+import datascript from 'datascript'
+import createDBConn from '../lib/createDBConn'
+import { Socket } from 'phoenix'
+import Channel from './channel'
+import url from './url'
+
 import Meteor from 'react-native-meteor';
 import { AppRegistry } from 'react-native';
 import {
@@ -8,8 +14,48 @@ import {
 
 // Meteor.connect('ws://localhost:3000/websocket');
 
+const NAMES = ['Girl', 'Boy', 'Horse', 'Foo', 'Face', 'Giant', 'Super', 'Bug', 'Captain', 'Lazer']
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
+const getRandomName = () => NAMES[getRandomInt(0, NAMES.length)]
+const getRandomUser = () => `${ getRandomName() }${ getRandomName() }${ getRandomName() }`
+const me = getRandomUser()
+
+const socket = new Socket(url)
+const conn = createDBConn()
+const transact = datascript.transact
+var log = []
+var meta = []
+
+// fires when we receive a message
+const receiveChatMessage = (conn, message) => {
+  const user = message.user
+  const isMe = (someUser) => me === someUser
+  console.log(JSON.stringify(message))
+
+  if (isMe(user)) return // prevent echoing yourself (TODO: server could handle this i guess?)
+
+  transact(conn, message.body, {'remote': true})
+}
+
+const channel = Channel(conn, me, receiveChatMessage)
+
+// fires when we transact data
+datascript.listen(conn, {channel}, function(report) {
+  log.push(report.tx_data)
+  meta.push(report.tx_meta)
+
+  if (report.tx_meta && report.tx_meta.remote) return
+
+  channel.send(report.tx_data)
+})
+
 export default function () {
   return {
+    socket: socket,
+    conn: conn,
+    transact: transact,
+    log: log,
+    meta: meta,
     Meteor,
     Router,
     Scene,
