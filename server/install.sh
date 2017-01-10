a5ff33fe6f8b54bd07bcafafaa3297db3a4caa7a
anonymous <anonymous>
2017-01-10 17:42:04 +0000

    misc

--- server/install.sh
+++ server/install.sh
@@ -3,7 +3,8 @@ wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo 
 git clone https://github.com/venturecommunism/stack.git
 cd stack/server
 cp config/example-config.exs config/config.exs
-sed -i -e 's/xx.xxx.xxx.xxx/localhost/g' config/config.exs
+IP_ADDR=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+sed -i -e "s/xx.xxx.xxx.xxx/$IP_ADDR/g" config/config.exs
 mix local.hex --force
 mix local.rebar --force
-mix deps.get -y
+mix deps.get
