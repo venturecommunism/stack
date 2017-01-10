e2d0d021132c48a10a0aad34caa23a5eb48be2a8
anonymous <anonymous>
2017-01-10 21:49:09 +0000

    misc

--- server/install.sh
+++ server/install.sh
@@ -14,17 +14,19 @@ cd stack/server
 mix local.hex --force
 mix local.rebar --force
 mix deps.get
-rm -r stack/server/deps/datomic_gen_server/priv/datomic_gen_server_peer
-cp -r stack/server-extras/datomic_gen_server_peer stack/server/deps/datomic_gen_server/priv
-echo 'cd stack/server/deps/datomic_gen_server/priv/datomic_gen_server_peer'
-echo 'lein uberjar'
-echo 'To find the container id do:'
-echo 'docker ps'
+rm -r deps/datomic_gen_server/priv/datomic_gen_server_peer
+cp -r ../server-extras/datomic_gen_server_peer deps/datomic_gen_server/priv
+cd deps/datomic_gen_server/priv/datomic_gen_server_peer
+lein uberjar
+echo 'Container ID for your running datomic can be found here:'
+docker ps
 echo 'Then, using the container id do:'
 echo 'docker exec -it CONTAINER-ID /bin/bash'
 echo 'root@CONTAINER-ID:/datomic-free-0.9.5530# bin/groovysh'
 echo 'groovy:000> import datomic.Peer'
 echo 'groovy:000> uri = "datomic:free://localhost:4334/responsive-db"'
 echo 'groovy:000> Peer.createDatabase(uri)'
-echo 'exit the groovy shell and datomic container'
+echo 'exit the groovy shell and datomic container (exit, exit)'
+echo 'cd stack/server'
+echo 'mix phoenix.server'
 echo 'IP Address is:' $IP_ADDR
