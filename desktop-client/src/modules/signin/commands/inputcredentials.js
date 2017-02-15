2aea8fb0467e937b06ff4fed533dfdfcd17d11d2
anonymous <anonymous>
2017-02-15 03:48:04 -0500

    client side sign-in

--- desktop-client/src/modules/signin/commands/inputcredentials.js
+++ desktop-client/src/modules/signin/commands/inputcredentials.js
@@ -1,5 +1,18 @@
+import {KJUR, KEYUTIL, b64utoutf8} from 'jsrsasign'
+import publickey from '../../../configs/publickey'
+
 export default {
   inputcredentials({conn, transact, log, meta}, ref, result) {
+    const pubkey = KEYUTIL.getKey(publickey)
+    const isValid = KJUR.jws.JWS.verifyJWT(ref, pubkey, {alg: ['RS256']})
+
+    if (!isValid) {
+      console.log('unauthorized attempt')
+      return
+    } else {
+      console.log('verified')
+    }
+
     transact(conn, [{':db/add': -1, 'name': 'Credentials', 'app/credentials': ref }])
   },
 }
