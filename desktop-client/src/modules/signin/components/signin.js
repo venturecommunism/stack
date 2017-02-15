6b4b479f3bc92faaa4c58dc6a0a579c9a6c5aa21
anonymous <anonymous>
2017-02-14 23:52:47 -0500

    misc

--- desktop-client/src/modules/signin/components/signin.js
+++ desktop-client/src/modules/signin/components/signin.js
@@ -9,12 +9,6 @@ const GenericComponentContainer = DataContainer(GenericComponent)
 const QueryBuilderComponent = ({ result, actions }) => (
   <div>
     <InputCredentials result={result} actions={actions} />
-    {result.map( s =>
-      <div key={s[0]}>
-        <h3>{s[0]}: {s[1]}</h3>
-        <GenericComponentContainer query={s[2]} />
-      </div>
-    )}
   </div>
 )
 
