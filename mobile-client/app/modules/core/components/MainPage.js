d6a06f3afdab2605a1f774bd861da53819b00e72
anonymous <anonymous>
2017-02-23 19:56:31 -0500

    disabling webtrc video since it doesn't work now anyway

--- mobile-client/app/modules/core/components/MainPage.js
+++ mobile-client/app/modules/core/components/MainPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Demo from '../../demo/components/index'
 import TwitterPlus from '../../twitterplus/components/index'
-import WebRTCVideo from '../../webrtcvideo/components/index'
+// import WebRTCVideo from '../../webrtcvideo/components/index'
 
 import {
   View,
@@ -47,7 +47,7 @@ class MainPage extends Component {
       <TopPad>
         <ScrollableTabContainer>
           <ScrollableTabCard tabLabel="Kickstarters">
-<WebRTCVideo />
+{/* <WebRTCVideo /> */}
 <Demo />
             <Text>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
