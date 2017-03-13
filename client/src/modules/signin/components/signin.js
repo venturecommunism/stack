0517ba80c4f07abefc80f02cb686542cbd512b81
anonymous <anonymous>
2017-03-13 18:35:29 +0000

    misc - removing to clean up logging

--- client/src/modules/signin/components/signin.js
+++ client/src/modules/signin/components/signin.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-import PeersContainer from '../../core/containers/peerscontainer'
+// import PeersContainer from '../../core/containers/peerscontainer'
 import DataContainer from '../../core/containers/datacontainer'
 import GenericComponent from './genericcomponent'
 import InputCredentials from './inputcredentials'
 
-const InputCredentialsContainer = PeersContainer(InputCredentials)
+// const InputCredentialsContainer = PeersContainer(InputCredentials)
 const GenericComponentContainer = DataContainer(GenericComponent)
 
 const QueryBuilderComponent = ({ result, actions }) => (
-  <div>
-    <InputCredentialsContainer result={result} actions={actions} />
+  <div>test
+    {/* <InputCredentialsContainer result={result} actions={actions} /> */}
   </div>
 )
 
