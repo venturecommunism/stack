7e4e930715cb6f286288a451f90117a9da418b5b
anonymous <anonymous>
2017-03-13 17:38:01 +0000

    putting JWT token in separate file

--- client/src/configs/context.js
+++ client/src/configs/context.js
@@ -4,9 +4,9 @@ import { Socket } from 'phoenix'
 import Channel from './channel'
 import url from './url'
 import publickey from './publickey'
+import creds from './creds'
 
 import {KJUR, KEYUTIL, b64utoutf8} from 'jsrsasign'
-const creds = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vZm9vLmNvbSIsInN1YiI6Im1haWx0bzptaWtlQGZvby5jb20iLCJuYmYiOjE0ODkyNzIwNTMsImlhdCI6MTQ4OTI3MjA1MywiZXhwIjoxNDg5MzU4NDUzLCJqdGkiOiJpZDEyMzQ1NiIsImF1ZCI6Imh0dHA6Ly9mb28uY29tL2VtcGxveWVlIn0.a6y1RjGhasZzwQbFKAYKF3Ft_fshu60tCHPaEFzbu_U-0FHffjDx0cFiBPvtwgNjKD7U2T_I2r_IU-BHayp4sInDaOGgwEns0q_uoXFX8gzTFOvdGcaDuVIEZhmt7lHaueQp2BqiYtk3iJFLI34Z6T3hPl3ER_-X0UYZEh9NwWHgNWsO0w16ODqsABJBb8FqZMb5n92vcr6doJd_lB-mfJoUtwsBlHXsssXnYENPgeGQ5SMjH71bNZJ06SSVyqLOvu6yC51BYQBQsf6a-iyp3yv3jVodQqE077pztXzD7Ra6YI7B5AqnJ3c9nFgWK1nDgQKSosFMmXiY7oq-FqGQ2Q'
 
 import Peer from 'peerjs'
 
