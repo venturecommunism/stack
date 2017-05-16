b0c0028b7833be1c729b721e3fa3befbc718c321
anonymous <anonymous>
2017-05-16 15:32:30 +0000

    restoring old dev config

--- server/apps/webrtc_example/config/dev.exs
+++ server/apps/webrtc_example/config/dev.exs
@@ -6,23 +6,23 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
-#config :webrtc_example, WebrtcExample.Endpoint,
-#  http: [port: 4000],
-#  debug_errors: true,
-#  code_reloader: true,
-#  cache_static_lookup: false,
-#  check_origin: false,
-#  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
+config :webrtc_example, WebrtcExample.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  cache_static_lookup: false,
+  check_origin: false,
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
 
 # Watch static and templates for browser reloading.
-#config :webrtc_example, WebrtcExample.Endpoint,
-#  live_reload: [
-#    patterns: [
-#      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
-#      ~r{web/views/.*(ex)$},
-#      ~r{web/templates/.*(eex)$}
-#    ]
-#  ]
+config :webrtc_example, WebrtcExample.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{web/views/.*(ex)$},
+      ~r{web/templates/.*(eex)$}
+    ]
+  ]
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
