6860fd6fc8b620a43d581bb607d34f22e69643c6
fsociety <anon@anon.com>
2017-05-17 05:51:34 -0400

    pure datascript with no react 'provider' containers

--- client/src/modules/core/routes.js
+++ client/src/modules/core/routes.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import {mount} from 'react-mounter'
-import { DBConnProvider } from '../../lib/react-datascript'
 
 import MainLayout from '../layout/components/main_layout'
 
 export default function (injectDeps, context, actions) {
-  const conn = context.conn
+// conn = context.conn
   const MainLayoutCtx = function (props) {
     const MainLayoutCtx = injectDeps(MainLayout)
     return (
-      <DBConnProvider conn={ conn } >
-        <MainLayoutCtx { ...props } />
-      </DBConnProvider>
+      <MainLayoutCtx { ...props } />
     )
   }
-  mount(MainLayoutCtx, {
-    conn: conn
-  })
+  mount(MainLayoutCtx)
 }
--- client/src/modules/demo/components/index.jsx
+++ client/src/modules/demo/components/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 
-// import RnDataContainer from '../../core/containers/rncontainer'
-// import AudioVideoContainer from '../../core/containers/audiovideocontainer'
-// import PeersContainer from '../../core/containers/peerscontainer'
 import DataContainer from '../../core/containers/datacontainer'
 import ActionsMapper from '../../core/containers/actionsmapper'
-
-import JWT from '../../clientjwt/components/jwt'
-import ImportUI from '../../jsonld/components/index.jsx'
 
 import AllUsersComponent from './allusers'
 import AllUserEdgesComponent from './alluseredges'
@@ -15,36 +9,20 @@ import FollowerTreeComponent from './followertree'
 
 import allUserQuery from '../queries/alluser'
 
-// import AudioVideoComponent from './audiovideo'
-// const AudioVideo = AudioVideoContainer(ActionsMapper('peers', AudioVideoComponent))
-
-import PeersComponent from './peers'
-// const AllPeersContainer = PeersContainer(ActionsMapper('peers', PeersComponent))
-
 import PlainResultComponent from './plainresult'
 const AllUsersDataContainer = DataContainer(PlainResultComponent)
 
-// const RnData = RnDataContainer(PlainResultComponent)
+const AllUsersFromIndex = DataContainer(AllUsersComponent)
 
-import allUsersFromIndex from '../queries/allusersfromindex'
-const AllUsersFromIndex = allUsersFromIndex(AllUsersComponent)
+const AllUserEdges = DataContainer(AllUserEdgesComponent)
 
-import allUserEdgesQuery from '../queries/alluseredges'
-const AllUserEdges = allUserEdgesQuery(AllUserEdgesComponent)
-
-import followerTreePullQuery from '../queries/followertreepull'
-const FollowerTree = followerTreePullQuery(ActionsMapper('followertree', FollowerTreeComponent))
+const FollowerTree = ActionsMapper('followertree', FollowerTreeComponent)
 
 const Demo = () => (
   <div>
-    {/* <AudioVideo /> */}
-    {/* <AllPeersContainer /> */}
     <AllUsersDataContainer query={allUserQuery} />
-    {/* <RnData /> */}
-    <JWT />
-    <ImportUI />
-    <AllUsersFromIndex />
-    <AllUserEdges />
+    <AllUsersFromIndex query={allUserQuery} />
+    <AllUserEdges query={allUserQuery} />
     <FollowerTree entityIds={[['name', 'Jane']]} />
   </div>
 )
